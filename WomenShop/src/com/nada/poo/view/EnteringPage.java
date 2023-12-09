6a753df73532329e049dd0d3b4208212be675237
Mélanie <melxhern@gmail.com>
2023-12-09 23:26:09 +0100

    creation de l'interface

---
+++ WomenShop/src/com/nada/poo/view/EnteringPage.java
@@ -0,0 +1,18 @@
+package com.nada.poo.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class EnteringPage {
+
+    @FXML
+    private Button enterbtn;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Text welcometext;
+}
