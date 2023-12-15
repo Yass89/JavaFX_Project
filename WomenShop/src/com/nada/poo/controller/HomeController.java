0b7e8a78129a6fff0ceeeea9f99d860cc18261c5
Yass89 <82099171+Yass89@users.noreply.github.com>
2023-12-15 05:35:18 +0100

    HomePage done
    - filter operational
    - showing product datas on a table view
    - displaying financials datas

--- WomenShop/src/com/nada/poo/controller/HomeController.java
+++ WomenShop/src/com/nada/poo/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.nada.poo.controller;
 
+import com.nada.poo.dao.FinancialDaoImpl;
 import com.nada.poo.dao.ProductDaoImpl;
 import com.nada.poo.database.DatabaseUtil;
 import com.nada.poo.model.Product;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -10,8 +12,10 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -50,9 +54,14 @@ public class HomeController extends PageController {
     // execute when the page is loaded
     @FXML
     public void initialize() {
-        /*loadProducts();
-        fillcategorySelector();
-        fillFinancialFields();*/
+        try {
+            loadProducts();
+            fillcategorySelector();
+            fillFinancialFields();
+        }
+        catch (SQLException e) {
+            showError(e.getMessage());
+        }
     }
 
     // fill the category selector with the categories from the database
@@ -62,25 +71,50 @@ public class HomeController extends PageController {
             return;
         }
         ObservableList<String> observableList = FXCollections.observableArrayList(categories);
+        // add " everything " to the list
+        observableList.add(0, "Everything");
         categorySelector.setItems(observableList);
     }
 
     // fill the table with the products from the database
-    public void loadProducts() throws SQLException {
-        List<Product> products = ProductDaoImpl.getProductsFromDatabase();
+    private void loadProducts() throws SQLException {
+        // Get products from database and load them into the table
+        List<Product> products = ProductDaoImpl.getProducts();
         if (products == null) {
             return;
         }
         ObservableList<Product> observableList = FXCollections.observableArrayList(products);
         productsTableHome.setItems(observableList);
+        displayProductsInTable();
+    }
+
+    // display the products in the table
+    public void displayProductsInTable() {
+        idColumnHome.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameColumnHome.setCellValueFactory(new PropertyValueFactory<>("name"));
+        typeColumnHome.setCellValueFactory(new PropertyValueFactory<>("type"));
+        discountColumnHome.setCellValueFactory(cellData ->
+                new SimpleStringProperty(cellData.getValue().isDiscounted() ? "Yes" : "No"));
+        sizeColumnHome.setCellValueFactory(cellData -> {
+            if (cellData.getValue().getCategoryId() == 1 || cellData.getValue().getCategoryId() == 2) {
+                return new SimpleStringProperty(String.valueOf((cellData.getValue()).getSize()));
+            }
+            return new SimpleStringProperty("");
+        });
+        priceColumnHome.setCellValueFactory(new PropertyValueFactory<>("price"));
+        stockColumnHome.setCellValueFactory(new PropertyValueFactory<>("nbItems"));
     }
 
     // filter the table by category
     @FXML
-    public void filterByCategory(ActionEvent event) throws SQLException {
+    protected void filterByCategory() throws SQLException {
         // get the selected category from the category selector
         String category = categorySelector.getSelectionModel().getSelectedItem();
         // show the products from the selected category
+        if (category.equals("Everything")) {
+            loadProducts();
+            return;
+        }
         List<Product> products = ProductDaoImpl.getProductsByCategoryFromDatabase(category);
         if (products == null) {
             return;
@@ -90,9 +124,14 @@ public class HomeController extends PageController {
     }
 
     // show financial data from the database
-    public void fillFinancialFields() {
+    public void fillFinancialFields() throws SQLException {
+        //
+        FinancialDaoImpl.createFinancials();
+        // connect to the database
+        Connection connection = DatabaseUtil.getConnection();
         // get the financial data from the database
-        ResultSet resultSet = DatabaseUtil.executeQuery("SELECT * FROM financials");
+        String sql = "SELECT * FROM financials";
+        ResultSet resultSet = connection.createStatement().executeQuery(sql);
         if (resultSet == null) {
             return;
         }
--- WomenShop/src/com/nada/poo/dao/FinancialDaoImpl.java
+++ WomenShop/src/com/nada/poo/dao/FinancialDaoImpl.java
@@ -1,9 +1,15 @@
 package com.nada.poo.dao;
 
 import com.nada.poo.database.DatabaseUtil;
+import com.nada.poo.model.Financial;
 
 public class FinancialDaoImpl {
 
+    public static void createFinancials(){
+        String sql = "INSERT INTO financials (capital, total_income, total_cost) VALUES ("+ Financial.capital+","+Financial.income+","
+                +Financial.cost+")";
+        DatabaseUtil.executeUpdate(sql);
+    }
     // update capital
     public static void updateCapitalDatabase(){
         DatabaseUtil.executeUpdate("UPDATE financials SET capital = "+com.nada.poo.model.Financial.capital+"Where financial_id = 1");
